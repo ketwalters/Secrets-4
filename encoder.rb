21eefecf8a9238caeb00d99479c91b9cd32602f4
David Gabeau <djg400@nyu.edu>
2013-11-14 16:25:54 -0500

    secrets

---
+++ encoder.rb
@@ -0,0 +1,25 @@
+
+secret = "Coding is a lot of fun"
+
+reversed_secret = secret.reverse  
+
+step2_string = reversed_secret.upcase 
+
+step3_string = step2_string.concat
+
+puts step3_string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
